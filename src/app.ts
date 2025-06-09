7b58851bd376a3f6029b53a502db8c39be0cf93a
Fahad <79867157+ShahandFahad@users.noreply.github.com>
2025-06-09 14:24:05 +0500

    chore add chore pkg, and enabled cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express, { Express, Request, Response } from 'express';
 const morgan = require('morgan');
 import chatRoutes from './routes/chat.routes';
 import authRoutes from './routes/auth.routes';
+const cors = require('cors');
 
 // init express app
 const app: Express = express();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
